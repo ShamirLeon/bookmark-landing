43e14d711f00f78f714b10166bcb003e338a4a96
ShamirLeon <leonshamir1@gmail.com>
2023-01-10 20:47:53 -0500

    initial project set-up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+	return (
+		<div className='App'>
+			<h1 className=''>
+				Hola mundo
+			</h1>
+		</div>
+	);
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			colors:{
+				'SoftBlue':'#5267DF',
+				'SoftRed':'#F95656',
+				'GrayishBlue': '#A9ABB6',
+				'DarkBlue':'#242B46'
+			},
+			fontFamily:{
+				'Rubik':'Rubik'
+			}
+		},
+	},
+	plugins: [],
+};
