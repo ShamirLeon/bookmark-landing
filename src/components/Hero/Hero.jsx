0cfd1d1adc8301dd78b0f84e7a2d4ab11497a2ef
ShamirLeon <leonshamir1@gmail.com>
2023-01-21 12:34:11 -0500

    Hero - Desktop Design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,30 +3,32 @@ import IllustrationHero from '../../assets/icons/illustration-hero.svg';
 
 const Hero = params => {
 	return (
-		<main className='my-8'>
+		<main className='my-8 flex-row-reverse items-center justify-around lg:flex lg:gap-12 lg:px-32'>
 			<div className='relative'>
 				<img
 					src={IllustrationHero}
 					alt='Ilustration Hero Bookmark'
-					className='scale-90'
+					className='scale-90 lg:scale-100'
 				/>
-				<div className='absolute right-0 top-16 -z-50 h-[70%] w-[70%] rounded-l-full bg-SoftBlue'></div>
 			</div>
-			<div className='flex flex-col gap-5 px-7 text-center'>
-				<h1 className='text-2xl  font-bold text-DarkBlue'>{hero.title}</h1>
+			<div className='absolute right-0 top-[200px] -z-50 h-[170px] w-[250px] rounded-l-full bg-SoftBlue lg:top-[230px] lg:h-[300px] lg:w-[500px]'></div>
+			<div className='flex flex-col gap-5 px-7 text-center lg:max-w-[500px] lg:px-0 lg:text-left'>
+				<h1 className='text-2xl font-bold text-DarkBlue lg:text-5xl lg:font-normal'>
+					{hero.title}
+				</h1>
 				<p className='text-base text-GrayishBlue'>{hero.description}</p>
 
 				<div className='my-8'>
 					<a
 						href='#'
-						className='mx-2 rounded-md bg-SoftBlue py-3 px-3 text-sm font-medium text-white shadow-lg'
+						className='mx-2 rounded-md bg-SoftBlue py-3 px-3 text-sm font-medium text-white shadow-lg lg:mx-0'
 					>
 						Get it on Chrome
 					</a>
 
 					<a
 						href='#'
-						className='mx-2 rounded-md bg-SoftBlue py-3 px-3 text-sm font-medium text-white shadow-lg'
+						className='mx-2 rounded-md py-3 px-3 text-sm font-medium text-GrayishBlue shadow-lg lg:mx-5'
 					>
 						Get it on firefox
 					</a>
